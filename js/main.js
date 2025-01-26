fa4a44c8b05246db9f1709314c4accee1e109cad
natanael <nathaxx09@hotmail.com>
2025-01-26 10:57:21 -0300

    ajustes en js para menu desplegable, correccion de margenes en version movil

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 /* menu */
 const menu = document.getElementById('menu')
 const menuBtn = document.getElementById('menu-btn')
+const menuLinks = document.querySelectorAll('.menu-item a');
 menuBtn.addEventListener('click', ()=>{
     menuBtn.classList.toggle('activo')
     menu.classList.toggle('mostrar-menu')
 })
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      menuBtn.classList.remove('activo'); // Elimina la clase 'activo' del botón
+      menu.classList.remove('mostrar-menu'); // Elimina la clase 'mostrar-menu' del menú
+    });
+  });
